3c229720ae4b760940ded31a9d052245c54b9273
moin-dbud <mohinsheikh47698@gmail.com>
2026-03-17 10:00:00 +0530

    feat: initialize project structure with core pages, components, and sections for the new site.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Work from './pages/Work';
 import Blogs from './pages/Blogs';
+import BlogDetail from './pages/BlogDetail';
 import Links from './pages/Links';
+import Guestbook from './pages/Guestbook';
+import Labs from './pages/Labs';
 import SmoothScroll from './components/SmoothScroll';
 import Navbar from './components/Navbar';
 import { FloodTransitionProvider, PageTransition } from './components/PageTransition';
@@ -20,7 +23,10 @@ function App() {
             <Route path="/about" element={<PageTransition><About /></PageTransition>} />
             <Route path="/work" element={<PageTransition><Work /></PageTransition>} />
             <Route path="/blogs" element={<PageTransition><Blogs /></PageTransition>} />
+            <Route path="/blog/:slug" element={<PageTransition><BlogDetail /></PageTransition>} />
             <Route path="/links" element={<PageTransition><Links /></PageTransition>} />
+            <Route path="/guestbook" element={<PageTransition><Guestbook /></PageTransition>} />
+            <Route path="/labs" element={<PageTransition><Labs /></PageTransition>} />
           </Routes>
         </FloodTransitionProvider>
       </BrowserRouter>
