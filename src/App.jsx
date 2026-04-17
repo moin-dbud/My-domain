6358398b134d6aec457b2983c19c5334f7538edd
moin-dbud <mohinsheikh47698@gmail.com>
2026-04-17 21:47:55 +0530

    updated speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Home from './pages/Home';
 import About from './pages/About';
 import Work from './pages/Work';
